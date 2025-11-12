6903252d85356f1e71df3abc1f82039191fcaadb
Bartlomiej <g00428194@atu.ie>
2025-11-12 12:42:34 +0000

    Add address input

---
+++ src/main/java/ie/atu/week7sem1/jpawithspringbootandh2/Address.java
@@ -0,0 +1,24 @@
+package ie.atu.week7sem1.jpawithspringbootandh2;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String county;
+
+    @NotBlank
+    private String eircode;
+
+    public Address(String eircode) {
+        this.eircode = eircode;
+        this.county = county;
+    }
+}
