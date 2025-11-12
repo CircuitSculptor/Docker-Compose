6cdfb19e368735451f0a1dbe0da8100440f09462
Bartlomiej <g00428194@atu.ie>
2025-11-12 12:58:09 +0000

    Add misisng lombok and entity code

--- src/main/java/ie/atu/week7sem1/jpawithspringbootandh2/Address.java
+++ src/main/java/ie/atu/week7sem1/jpawithspringbootandh2/Address.java
@@ -1,10 +1,17 @@
 package ie.atu.week7sem1.jpawithspringbootandh2;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 public class Address {
 
     @Id
